d4bb9b415ebe28762b58f5c374ad8d7e49ad9de4
moiseiyaa <moiseiyaa@gmail.com>
2026-01-12 08:48:37 +0200

    changed the image format

--- src/scripts/seedTrips.ts
+++ src/scripts/seedTrips.ts
@@ -207,7 +207,7 @@ const TRIPS: TripSeed[] = [
       'Local art appreciation'
     ],
     images: [
-      '/images/trips/douala-city-1.jpg',
+      '/images/trips/douala-city-1.webp',
       '/images/trips/douala-city-2.jpg',
       '/images/trips/douala-city-3.jpg',
       '/images/trips/douala-city-4.jpg'
